a18273f770bc47c2669ef9cca60e856500c98121
udontur <h@hadrian.cc>
2025-06-20 10:56:44 +0800

    WRK: 2025-06-20 10:56:44

--- home/term/yazi.nix
+++ home/term/yazi.nix
@@ -7,13 +7,13 @@
   xdg.configFile."yazi/yazi.toml".text=''
     [opener]
     play = [
-    	{ run = 'hx "$@"', orphan = true},
+    	{ run = 'zen "$@"', orphan = true},
     ]
     edit = [
     	{ run = 'hx "$@"', block = true},
     ]
     open = [
-    	{ run = 'xdg-open "$@"', desc = "Open" },
+    	{ run = 'zen "$@"', desc = "Open" },
     ]
 
     [mgr]
