b4d2087a6fa080dd839df90c2f59ceff56c871b3
udontur <h@hadrian.cc>
2025-06-20 11:32:10 +0800

    WRK: 2025-06-20 11:32:10

--- home/hyprland/workstation/hyprland.nix
+++ home/hyprland/workstation/hyprland.nix
@@ -17,15 +17,15 @@
   ];
   wayland.windowManager.hyprland = {
     enable = true;
+    plugins = [
+      inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
+    ];
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland; 
     extraConfig = ''
       source = ~/.config/hypr/modules/keybinds.conf
       source = ~/.config/hypr/modules/hyprwm.conf
       source = ~/.config/hypr/modules/system.conf
       source = ~/.config/hypr/modules/others.conf
     '';
-    plugins = [
-      inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
-    ];
-
   };
 }
