f871140dea64cc0789b10ec73bb57f97f3132a30
udontur <h@hadrian.cc>
2025-06-25 20:48:38 +0800

    WRK: 2025-06-25 20:48:38

--- nixos/workstation.nix
+++ nixos/workstation.nix
@@ -17,5 +17,4 @@
   ];
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
-  virtualisation.virtualbox.host.enableExtensionPack = true;
 }
