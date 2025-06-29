3afa3d120256e5ad81584e4d09fd9bfec2270510
udontur <h@hadrian.cc>
2025-06-29 20:35:48 +0800

    LAP: 2025-06-29 20:35:48

--- home/env/battery.nix
+++ home/env/battery.nix
@@ -9,11 +9,11 @@
 
             if [[ "$battery_level" -ge 70 ]]; then
                 notify-send "Just a heads up" "Battery is at 70%"
-            elif [[ "$battery_level" -le 50 && "$battery_level" -gt 30]]; then
+            elif [[ "$battery_level" -le 50 && "$battery_level" -gt 30 ]]; then
                 notify-send "Battery is half full" "Battery is at 50%, please plug it in. "
             elif [[ "$battery_level" -le 30 && "$battery_level" -gt 20 ]]; then
                 notify-send "就嚟冇電" "Battery is at 30%, you need to charge it now!"
-            elif [[ "$battery_level" -le 20 && "$battery_level" -gt 10]]; then
+            elif [[ "$battery_level" -le 20 && "$battery_level" -gt 10 ]]; then
                 notify-send "Its suffering... 冇電喇" "Battery is at 20%, plug it in right now!"
             elif [[ "$battery_level" -le 10 ]]; then
                 notify-send "真係冇電喇!!!" "得返10%"
