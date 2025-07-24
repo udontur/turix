8e6a62b6ec941d1d1347bc8adf4707ab6a076581
udontur <h@hadrian.cc>
2025-07-24 19:32:34 +0800

    LAP: 2025-07-24 19:32:34

--- home/hyprland/spec/laptop.nix
+++ home/hyprland/spec/laptop.nix
@@ -20,9 +20,6 @@
 
         # Monitor
         monitor=eDP-1, 1920x1200@60, 0x0, 1.25
-        decoration {
-            screen_shader = /home/udontur/.config/hypr/modules/nightLight.glsl
-        }
 
         # Trackpad
         device{
@@ -37,28 +34,6 @@
               natural_scroll=true
               scroll_factor=0.15
             }
-        }
-      '';
-    };
-  };
-  home.file = {
-    "/home/udontur/.config/hypr/modules/nightLight.glsl" = {
-      text = ''
-        precision mediump float;
-        varying vec2 v_texcoord;
-        uniform sampler2D tex;
-
-        void main() {
-
-            vec4 pixColor = texture2D(tex, v_texcoord);
-
-            // green
-            pixColor[1] *= 0.75;
-
-            // blue
-            pixColor[2] *= 0.55;
-
-            gl_FragColor = pixColor;
         }
       '';
     };
