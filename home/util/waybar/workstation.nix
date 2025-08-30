295a0b0a585f4e12710afdbafed89d97ebe6cc84
udontur <h@hadrian.cc>
2025-08-30 11:44:07 +0800

    WRK: 2025-08-30 11:44:07

--- home/util/waybar/workstation.nix
+++ home/util/waybar/workstation.nix
@@ -44,7 +44,8 @@
         border-radius: 6px;
         color: #ffffff;
         background: linear-gradient(90deg,rgba(0, 242, 96, 1) 0%, rgba(5, 117, 230, 1) 100%);
-        padding: 0.3rem 0.5rem;
+        padding: 0.3rem 0.6rem;
+        margin: 0rem 0.3rem;
       }
     '';
     settings = [{
