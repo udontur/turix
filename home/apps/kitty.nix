ff338b477f678850d224a9bdc0513563928fea94
udontur <h@hadrian.cc>
2025-08-30 11:05:36 +0800

    WRK: 2025-08-30 11:05:36

--- home/apps/kitty.nix
+++ home/apps/kitty.nix
@@ -6,8 +6,9 @@
       background #2c2e34
       foreground #ebebeb
       font_family Recursive Mono Casual Static
-      font_size 12.0
-      background_opacity 0.5
+      font_size 11.0
+      background_opacity 0.8
+      background_blue 32
       confirm_os_window_close 0
     '';
   };
