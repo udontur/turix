cb64a40fff33db563e63f5ab696fe80f99d3dd51
udontur <h@hadrian.cc>
2025-09-20 10:58:46 +0800

    WRK: 2025-09-20 10:58:46

--- home/hyprland/spec/workstation.nix
+++ home/hyprland/spec/workstation.nix
@@ -12,7 +12,7 @@
       text=''
         # Monitor
         monitor=HDMI-A-1, 1920x1080@144, 0x0, 1
-        monitor=DP-1, 2560x1440@165, 1920x0, 1
+        monitor=DP-1, 2560x1440@165, 1920x0, 1.25
         
         # Suspend
         bind = SUPER_Shift, Delete, exec, systemctl suspend
