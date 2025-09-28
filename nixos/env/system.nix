2e488734fbe5e44106ff8b559f618b588770e1eb
udontur <h@hadrian.cc>
2025-09-28 16:38:37 +0800

    WRK: 2025-09-28 16:38:37

--- nixos/env/system.nix
+++ nixos/env/system.nix
@@ -9,7 +9,19 @@
 {
 
   # Bluetooth
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  settings = {
+    General = {
+      Experimental = true;
+      FastConnectable = true;
+    };
+    Policy = {
+      AutoEnable = true;
+    };
+  };
+};
   
   # Fix dynamically linked libraries
   programs.nix-ld.enable = true;
