6b97887190427cda3fa0d46a22d8c2fbaadf3e5b
udontur <h@hadrian.cc>
2025-11-15 20:20:29 +0800

    WRK: 2025-11-15 20:20:29

--- home/hyprland/modules/hyprwm.nix
+++ home/hyprland/modules/hyprwm.nix
@@ -16,15 +16,9 @@
         windowrulev2 = opacity 0.9, class:code
 
         # IDK what are these
-        # windowrulev2 = suppressevent maximize, class:.*
-        # windowrulev2 = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0
-        # windowrulev2 = rounding 16, floating:1
+        windowrulev2 = suppressevent maximize, class:.*
+        windowrulev2 = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0
 
-        # Dynamic window decoratins
-        # workspace = w[tv1], gapsout:0, gapsin:0
-        # workspace = f[1], gapsout:0, gapsin:0
-        # windowrulev2 = bordersize 0, floating:0, onworkspace:f[1]
-        # windowrulev2 = rounding 0, floating:0, onworkspace:f[1]
 
         # Hyprshot is broken
         layerrule = noanim, selection
