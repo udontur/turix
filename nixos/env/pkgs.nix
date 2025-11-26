ebb6d0e0c04b6457f2fc1bc857f99acce8a9c943
udontur <h@hadrian.cc>
2025-11-26 22:49:24 +0800

    WRK: 2025-11-26 22:49:24

--- nixos/env/pkgs.nix
+++ nixos/env/pkgs.nix
@@ -14,6 +14,14 @@
 
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
+
   # Virtual Machine
   virtualisation.vmware.host.enable = true;
 
