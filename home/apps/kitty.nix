bb83418a4f6c04cb448cee0d6b4946b119d22ea6
udontur <h@hadrian.cc>
2025-12-02 21:03:00 +0800

    WRK: 2025-12-02 21:03:00

--- home/apps/kitty.nix
+++ home/apps/kitty.nix
@@ -8,7 +8,7 @@
       font_family Recursive Mono Casual Static
       font_size 11.0
       background_opacity 0.5
-      background_blue 22
+      background_blur 22
       confirm_os_window_close 0
       window_padding_width 8
     '';
