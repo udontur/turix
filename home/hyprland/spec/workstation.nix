b622daa8f4b8405603768ec45bfc534fe7a7f005
udontur <h@hadrian.cc>
2026-01-15 22:24:39 +0800

    WRK: 2026-01-15 22:24:39

--- home/hyprland/spec/workstation.nix
+++ home/hyprland/spec/workstation.nix
@@ -10,9 +10,6 @@
   home.file = {
     "/home/udontur/.config/hypr/modules/spec.conf" = {
       text = ''
-        # Launch
-        exec-once = nohup easyeffects --gapplication-service
-
         # Monitor
         monitor=HDMI-A-1, 1920x1080@144, 0x0, 1
         monitor=DP-1, 2560x1440@165, 1920x0, 1.25
